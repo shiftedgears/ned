0ae3bc4713e701ca018998265d23897458c47c02
Nicholas hopkins <hopkins.n3@gmail.com>
2023-07-22 21:18:02 -0700

    change commands from strings to chars

--- ned.c
+++ ned.c
@@ -8,56 +8,66 @@ int main(int argc, char **argv){
    */
    int max_line_size = 200;
    int file_name_size = 255;
-   char quit[2];
-   strcpy(quit, "q");
-   char read[2];
-   strcpy(read, "r");
-   char load[2];
-   strcpy(load, "l");
+   char quit = 'q';
+   char read = 'r';
+   char load = 'l';
 
-   char cmd[10]; //var to hold user command
-   strcpy(cmd, "t");
+   char cmd = 't'; //var to hold user command
 
     FILE *f_r; //create file pointer for reading
-    FILE *f_wa; //file pointer for write/append
+    FILE *f_w; //file pointer for write/append
     
-    char file_name[255];
-    if(argc == 2){
+    char file_name[file_name_size];
+    if(argc == 2){ //if they loaded with a filename, load it into the file_name var
         strcpy(file_name, argv[1]);
     }
 
-   while(strcmp(cmd, quit) != 0){
-      
-      scanf("%s", cmd);
-      
-      if(strcmp(cmd, quit) == 0){ //if user wants to quit, break
-         break;
-      }
-      else if(strcmp(cmd, load) == 0){//loading a new file to read/write
-	scanf("%s", file_name); //wait for user to type the file to "load" (just changes active file_name)
-      }
-      else if(strcmp(cmd, read) == 0){ //if user wants to read file
-          
-          f_r = fopen(file_name, "r"); //open file in read mode
-          
-          if(!f_r){ //if there was no file to read, alert user
-              printf("!\n");
-          }
-          else{ //otherwise it exits. read it line by line
-              int line = 1;
-              char curr_line[max_line_size];
-              while(fgets(curr_line, max_line_size, f_r)){
-                  printf("%d | %s", line, curr_line);
-                  line++;
-              }
-              
-              fclose(f_r); //close file
-          }
-      }
-      else{
-         printf("?\n");
-      }
-   }
+    while(cmd != quit){
+    
+        scanf("%c", &cmd);
+
+        if(cmd == quit){ //if user wants to quit, break
+            break;
+        }
+        else if(cmd == load){//loading a new file to read/write
+            scanf("%s", file_name); //wait for user to type the file to "load" (just changes active file_name)
+            
+            //checks to see if file exists
+            f_r = fopen(file_name, "r"); //open file in read mode
+
+            if(!f_r){ //if there was no file to read, alert user
+                printf("!\n");
+            }
+            else{
+                fclose(f_r); //otherwise close it
+            }
+        }
+        else if(cmd == read){ //if user wants to read file
+
+            f_r = fopen(file_name, "r"); //open file in read mode
+
+            if(!f_r){ //if there was no file to read, alert user
+                printf("!\n");
+            }
+            else{ //otherwise it exists. read it line by line
+                int line = 1;
+                char curr_line[max_line_size];
+                while(fgets(curr_line, max_line_size, f_r)){
+                    printf("%d | %s", line, curr_line);
+                    line++;
+                }
+                
+                printf("\n");
+
+                //fclose(f_r); //close file
+            }
+        }
+        else{
+            printf("?\n");
+        }
+        
+        while ((cmd = getchar()) != '\n' && cmd != EOF) { }
+    }
 
    if(f_r){ //cleanup pointers
      fclose(f_r);
