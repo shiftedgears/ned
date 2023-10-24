60ddb31dae04ec26761aaecd2045f0ac3bb2d3ff
Nicholas Hopkins <107825158+shiftedgears@users.noreply.github.com>
2023-10-23 23:07:17 -0700

    working on appending

--- ned.c
+++ ned.c
@@ -6,17 +6,50 @@
 
 char file_name[MAX_FILE_NAME]; //holds global file_name for opening/closing
 
-void load_file(){
-    scanf("%s", file_name); //wait for user to type the file to "load" (just changes active file_name)
-            
-       //checks to see if file exists
-       FILE *f_r = fopen(file_name, "r"); //open file in read mode
+/*
+    Sets the global file name to "!" for quick detection of whether a file is loaded or not
+*/
+void unload_file(){
+    strcpy(file_name, "!");
+}
 
-       if(!f_r){ //if there was no file to read, alert user
-	       printf("!\n");
-       }
-       else fclose(f_r);
-       
+/*
+    Called from the "load file" case, 'l', where the user gives a file name
+    to load into the file_name var
+*/
+void load_file(char *f_name){
+   
+   strcpy(file_name, f_name);
+   //checks to see if file exists
+   FILE *f_r = fopen(file_name, "r"); //open file in read mode
+
+   if(!f_r){ //if there was no file to read, alert user. Create it
+    printf("%s: No such file name or directory\n", file_name);
+    f_r = fopen(f_name, "w+");
+   }
+   fclose(f_r); //close file pointer
+}
+
+/*
+    Called from the "read file" case, 'r', where we will dump the file into the terminal
+*/
+void read_file(){
+    FILE* f_r = fopen(file_name, "r"); //open file in read mode
+    char curr_line[MAX_LINE_SIZE];
+    
+    if(!f_r) printf("!\n"); //if there was no file to read, alert user
+    else{ //otherwise it exists. read it line by line
+        printf("F=|=%s\n", file_name); //output name of file
+        int line = 1;
+        while(fgets(curr_line, MAX_LINE_SIZE, f_r)){
+            printf("%d | %s", line, curr_line);
+            line++;
+        }
+        
+        printf("\n");
+   
+        fclose(f_r); //close file
+   }
 }
 
 int main(int argc, char **argv){
@@ -31,9 +64,14 @@ int main(int argc, char **argv){
    
    char cmd = 't'; //var to hold user command
    char text_app[MAX_LINE_SIZE]; //create c-string for whatever users wants to write
-   char curr_line[MAX_LINE_SIZE];
+   //char curr_line[MAX_LINE_SIZE];
+   char f_name[MAX_FILE_NAME];
+   
+   unload_file();
+   
+   if(argc == 2) load_file(argv[1]);
 
-   FILE *f_r; //create file pointer for reading
+   //FILE *f_r; //create file pointer for reading
    FILE *f_w; //file pointer for write/append
     
    if(argc == 2){ //if they loaded with a filename, load it into the file_name var
@@ -48,50 +86,36 @@ int main(int argc, char **argv){
              
      case 'q': //if user wants to quit, break
        break;
-     case 'l' ://loading a new file to read/write
-     
-       load_file();
+     case 'l' ://loading a file to read/write to. Writes over the previous file in the file_name
+       scanf("%s", f_name); //wait for user to type the file to "load" (just changes active file_name)
+            
+       load_file(f_name);
        
        break;
-     
      case 'r': //if user wants to read file
 
-       f_r = fopen(file_name, "r"); //open file in read mode
-
-       if(!f_r){ //if there was no file to read, alert user
-	       printf("!\n");
-       }
-       else{ //otherwise it exists. read it line by line
-           printf("F=|=%s\n", file_name);
-           int line = 1;
-           while(fgets(curr_line, MAX_LINE_SIZE, f_r)){
-            printf("%d | %s", line, curr_line);
-            line++;
-	       }
-                
-	       printf("\n");
-	       
-	       fclose(f_r); //close file
-       }
+       read_file();
+       
        break;
      
      case 'a':  //if user wants to append to a file
 
-       f_w = fopen(file_name, "a"); //open file in write mode
+       f_w = fopen(file_name, "a"); //open file in append only mode
 
-       if(!f_w){ //if no file opened
+       if(!f_w){ //if no file opened, alert user
 	       printf("!\n");
        }
        else{//otherwise it exists, let user type
 
-	       fflush(stdin);
+           flockfile (f_w); //lock the file
+	       //fflush(stdin);
 	       fgets(text_app, MAX_LINE_SIZE, stdin);
 	       
-	       printf("you typed %s", text_app);
+	       //printf("you typed %s", text_app);
 
 	       fputs(text_app, f_w);
-	       
 
+           funlockfile (f_w); //unlock the file
 	       fclose(f_w); //close pointer
 	    
        }
